c7c64badb9023b088534a221d282bbb76f2acceb
Letalandroid <carlossoncra@gmail.com>
2022-06-03 21:42:06 -0500

    3-6-22/21-42

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Home } from './pages/home';
 import { Popular } from './pages/popular/popular'
 import { Top } from './pages/tendencias/top'
+import { Page404 } from './pages/page404/404'
 
 function App() {
 
@@ -12,9 +13,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={ <Home /> } />
+          <Route path='/home' element={ <Home /> } />
           <Route path='/popular' element={ <Popular /> } />
           <Route path='/tendencias' element={ <Top /> } />
-          {/* <Route path='*' element={ <Page404 /> } /> */}
+          <Route path='*' element={ <Page404 /> } />
         </Routes>
       </BrowserRouter>
 
