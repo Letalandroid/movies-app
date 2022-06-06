918d679992122f57658dc5be646277d3dacbad77
Letalandroid <carlossoncra@gmail.com>
2022-06-06 10:41:41 -0500

    6-6-22/10-41

--- src/components/Main/poster/poster.js
+++ src/components/Main/poster/poster.js
@@ -2,9 +2,10 @@ import { React, useState } from 'react';
 import './poster.css';
 import { Fetching } from '../../../fetching';
 
+document.body.style.backgroundColor = '#212529';
+
 export const Poster = () => {
 
-    const poster = 'https://image.tmdb.org/t/p/w500';
     const [url] = useState('https://api.themoviedb.org/3/movie');
     const estado = Fetching(url, 'popular', process.env.REACT_APP_API_KEY, 'es-PE', 1);
     const {cargando, data} = estado;
@@ -15,11 +16,10 @@ export const Poster = () => {
             {
                 cargando ? <h1>Un momento por favor...</h1> :
 
-                <div div class = "card bg-dark text-white w-100 mb-0" >
-                    <img src={ `${ poster }${ data.results[0].backdrop_path }` } class="card-img opacity-25 w-100" alt='' />
-                    <div class="card-img-overlay">
-                        <h5 class="card-title display-2">{ data.results[0].original_title }</h5>
-                        <p class="card-text h4 mb-3">{ data.results[0].overview }</p>
+                <div div className = "w-100 mb-0" >
+                    <div className="tarjeta card-img-overlay">
+                        <h5 className="card-title display-2 text">{ data.results[0].original_title }</h5>
+                        <p className="card-text h4 mb-3 text">{ data.results[0].overview }</p>
                         <button type="button" class="btn btn-light">Ver Ahora</button>
                     </div>
                 </div>
